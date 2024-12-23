0deb83cc40e0cb49516bf17f3fdf5553946f3f9a
PbtinaDev <pbt@pbtservers.com>
2024-12-23 18:01:39 +0100

    Arreglar MOD_ID no actualizado...

--- common/src/main/java/com/nemonotfound/nemos/inventory/sorting/Constants.java
+++ common/src/main/java/com/nemonotfound/nemos/inventory/sorting/Constants.java
@@ -5,6 +5,6 @@ import org.slf4j.LoggerFactory;
 
 public class Constants {
 
-	public static final String MOD_ID = "nemos_inventory_sorting";
+	public static final String MOD_ID = "nemos_inventory_sorting_fix";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
 }
